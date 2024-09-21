777910590b962508a8fcc4f0eee995dda250b9bd
Ananta kandel <kandelananta12@gmail.com>
2024-09-21 22:58:24 +0545

    test

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const { PrismaClient } = require('@prisma/client');
 
+const prisma = new PrismaClient()
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/home", (req, res) => res.send("Hello on vercel"));
+app.post("/hello",async (req,res)=>{
+     const name = "ananta"
+     const email = "kandelananta12@gmail.com"
+     const user = await prisma.user.create({
+        data: {
+          name: name,
+          email: email
+        },
+      })
+      console.log(user)
+})
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
