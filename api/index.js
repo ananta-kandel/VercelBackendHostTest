3b5f972b434821a1123b8b6f59916899ad2c05eb
Ananta kandel <kandelananta12@gmail.com>
2024-09-21 22:35:54 +0545

    test

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
-const app =  require("../index")
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/home", (req, res) => res.send("Hello on vercel"));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
